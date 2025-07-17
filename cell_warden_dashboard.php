0f3babffad2b85cdb2b9fa644bb52242e80e4daf
Rodney-Omondi <rodney.ngaa@strathmore.edu>
2025-07-17 13:39:53 -0700

    Update cell_warden_dashboard.php

--- cell_warden_dashboard.php
+++ cell_warden_dashboard.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-echo "<h1>Welcome to the Admin Dashboard</h1>";
+echo "<h1>Welcome to the CELL WARDEN Dashboard</h1>";
 echo "<p>Logged in as: " . $_SESSION['email'] . "</p>";
 ?>
